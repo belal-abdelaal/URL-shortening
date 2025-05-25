8fd95ae76b3f8da27ed06ef9ce48876bb378741f
belal-abdelaal <belal.business.email@gmail.com>
2025-05-26 00:27:45 +0300

    feat(routes): Added the  endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller()->group(function () {});
+Route::controller(UserController::class)->group(function () {
+    Route::post("/users", "create");
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
