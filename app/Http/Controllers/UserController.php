32dece7568274fbed0038d86015fff92287720b8
belal-abdelaal <belal.business.email@gmail.com>
2025-05-26 00:16:31 +0300

    feat(app/Controllers): Added a  code response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function validate(UserRequest $request)
+    public function validate(UserRequest $request, $keys = ["name", "email", "password"])
     {
-        return $request->validated();
+        $data = $request->only($keys);
+        $data['password'] = Hash::make($data['password']);
+        return $data;
+    }
+    public function create(UserRequest $request)
+    {
+        $data = $this->validate($request);
+        $user = User::create($data);
+        if ($user) {
+            response([
+                "messsage" => "Account created successfuly",
+                "user" => [
+                    "name" => $user->name,
+                    "email" => $user->email
+                ]
+            ], 201);
+        } else {
+            response([
+                "messsage" => "Internal server error !"
+            ], 500);
+        }
     }
 }
